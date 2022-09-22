5772f05f30c161178c61be8fd5da68ac071e2abc
Takeshi Kawasaki <72239760+TakeshiKawasaki@users.noreply.github.com>
2022-09-22 12:59:57 +0900

    Update qsub.sh

--- Chapter0/qsub.sh
+++ Chapter0/qsub.sh
@@ -1,11 +1,12 @@
-#!/bin/bash                                                                                                    
-#                                                                                                              
-#$ -cwd                                                                                                        
-#$ -j y                                                                                                        
-#$ -S /bin/bash                                                                                                
-#$ -M kawasaki@r.phys.nagoya-u.ac.jp                                                                           
-#$ -m ea                                                                                                       
-#$ -V                                                                                                          
-#                                                                                                              
-#$ -q all.q@einstein                                                                                              
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -M kawasaki@r.phys.nagoya-u.ac.jp
+#$ -m ea
+#$ -V
+#                                                                                              
+#$ -q all.q@melon
+#$ -q all.q@budou
 ./pi.out
